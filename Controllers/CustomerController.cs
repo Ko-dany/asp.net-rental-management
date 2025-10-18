1ae41049772dfb5d0a16ec2d8095a0c4c82188c1
gabrielsiewert <gabrielsiewert1@gmail.com>
2025-10-17 22:57:33 -0400

    Created a view for the GetAllCustomers. It is now accessable through the AdminDashboard.cshtml.

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,10 +1,84 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Midterm_EquipmentRental.Models;
+using Midterm_EquipmentRental.Repositories;
 
 namespace Midterm_EquipmentRental.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CustomerController : ControllerBase
+    public class CustomerController : Controller // ALL USER ACCESS MUST ONLY ACCESS THE USERS DATA
     {
+        private readonly IUnitOfWork _context;
+
+        public CustomerController(IUnitOfWork context)
+        {
+            _context = context;
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetAllCustomers()
+        {
+            var customers = _context.Customers.GetAllCustomers();
+            return View("Index", customers);
+        }
+
+        //[Authorize]
+        [HttpGet("{id}")]
+        public ActionResult<Customer> GetCustomertById(int id)
+        {
+            var customer = _context.Customers.GetCustomerById(id);
+            return Ok(customer);
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            if (customer == null) { return BadRequest(); }
+            _context.Customers.AddCustomer(customer);
+            _context.Complete();
+            return Ok();
+        }
+
+        //[Authorize]
+        [HttpPut("{id}")]
+        public ActionResult UpdateCustomer(int id, Customer customer)
+        {
+            var existingCustomer = _context.Customers.GetCustomerById(id);
+            if (existingCustomer == null) { return NotFound("Customer with the given ID does not exist"); }
+            if (id != existingCustomer.Id) { return BadRequest("ID mismatch"); }
+            _context.Customers.UpdateCustomer(customer);
+            _context.Complete();
+            return Ok();
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCustomer(int id)
+        {
+            var existingCustomer = _context.Customers.GetCustomerById(id);
+            if (existingCustomer == null) { return NotFound("Customer with the given ID does not exist"); }
+            _context.Customers.DeleteCustomer(existingCustomer);
+            _context.Complete();
+            return Ok();
+        }
+
+        //[Authorize]
+        [HttpGet("{id}/rentals")]
+        public ActionResult<IEnumerable<Rental>> GetCustomerRentalHistoryById(int id)
+        {
+            var customerRentals = _context.Customers.GetCustomerRentalHistoryById(id);
+            return View("Index", customerRentals);
+        }
+
+        //[Authorize]
+        [HttpGet("{id}/active-rental")]
+        public ActionResult<IEnumerable<Rental>> GetCustomerActiveRentalsById(int id)
+        {
+            var customerActiveRentals = _context.Customers.GetCustomerActiveRentalsById(id);
+            return View("Index", customerActiveRentals);
+        }
     }
 }
+
--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -12,7 +12,37 @@ namespace Midterm_EquipmentRental.Repositories
         {
             _context = context;
         }
-
         public DbSet<Customer> GetCustomerDB() { return _context.Customers; }
+
+        public IEnumerable<Customer> GetAllCustomers() { return _context.Customers.ToList(); }
+
+        public Customer GetCustomerById(int id) { return _context.Customers.Find(id); }
+
+        public void AddCustomer(Customer customer) { _context.Customers.Add(customer); }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            Customer existingCustomer = GetCustomerById(customer.Id);
+            existingCustomer.Name = customer.Name;
+            existingCustomer.Email = customer.Email;
+            existingCustomer.UserName = customer.UserName;
+            existingCustomer.Password = customer.Password;  // AVOID ??
+            existingCustomer.Role = customer.Role;          // AVOID ??
+            existingCustomer.RentalHistory = customer.RentalHistory;
+        }
+
+        public void DeleteCustomer(Customer customer) { _context.Customers.Remove(customer); }
+
+        public IEnumerable<Rental> GetCustomerRentalHistoryById(int id)
+        {
+            Customer customer = _context.Customers.Find(id);
+            return customer.RentalHistory;
+        }
+
+        public IEnumerable<Rental> GetCustomerActiveRentalsById(int id)
+        {
+            Customer customer = _context.Customers.Find(id);
+            return customer.RentalHistory.Where(r => r.Status == RentalStatus.Active);
+        }
     }
 }
