4b8a371fc8c95d005533ac5051bda532d4fd3c48
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-10-18 19:24:18 -0400

    Connect dashboard to all index pages

--- Controllers/ViewController.cs
+++ Controllers/ViewController.cs
@@ -4,18 +4,29 @@ using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Midterm_EquipmentRental.Models;
 using Midterm_EquipmentRental.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Midterm_EquipmentRental.Models.ViewModels;
+using System.Data;
 
 namespace Midterm_EquipmentRental.Controllers
 {
     public class ViewController : Controller
     {
         private readonly IAuthService _authService;
+        private readonly IEquipmentService _equipmentService;
+        private readonly ICustomerService _customerService;
+        private readonly IRentalService _rentalService;
 
-        public ViewController(IAuthService authService)
+        public ViewController(IAuthService authService, IEquipmentService equipmentService, ICustomerService customerService, IRentalService rentalService)
         {
             _authService = authService;
+            _equipmentService = equipmentService;
+            _customerService = customerService;
+            _rentalService = rentalService;
         }
 
+        /* Authentication view */
         [HttpGet]
         public IActionResult Login()
         {
@@ -44,6 +55,11 @@ namespace Midterm_EquipmentRental.Controllers
             }
 
             var token = _authService.GenerateToken(user);
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+            var roleClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Role)?.Value;
+
+            HttpContext.Session.SetString("role", roleClaim);
             HttpContext.Session.SetString("JwtToken", token);
 
             return user.Role switch
@@ -58,33 +74,91 @@ namespace Midterm_EquipmentRental.Controllers
         [HttpGet]
         public IActionResult AdminDashboard()
         {
-            var token = HttpContext.Session.GetString("JwtToken");
-            if (string.IsNullOrEmpty(token)) return RedirectToAction("Login");
+            var role = HttpContext.Session.GetString("role");
+            if (string.IsNullOrEmpty(role)) return RedirectToAction("Login");
 
-            var handler = new JwtSecurityTokenHandler();
-            var jwtToken = handler.ReadJwtToken(token);
-            var roleClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Role)?.Value;
-
-            if (roleClaim != UserRole.Admin)
+            if (role != UserRole.Admin)
                 return RedirectToAction("Login");
 
-            return View();
+            return View("~/Views/Dashboard/Index.cshtml");
         }
 
         [HttpGet]
         public IActionResult UserDashboard()
         {
-            var token = HttpContext.Session.GetString("JwtToken");
-            if (string.IsNullOrEmpty(token)) return RedirectToAction("Login");
+            var role = HttpContext.Session.GetString("role");
+            if (string.IsNullOrEmpty(role)) return RedirectToAction("Login");
 
-            var handler = new JwtSecurityTokenHandler();
-            var jwtToken = handler.ReadJwtToken(token);
-            var roleClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Role)?.Value;
-
-            if (roleClaim != UserRole.User)
+            if (role != UserRole.User)
                 return RedirectToAction("Login");
 
-            return View();
+            return View("~/Views/Dashboard/Index.cshtml");
+        }
+
+        /* Equipments view */
+        [Authorize(Roles = $"{UserRole.Admin}, {UserRole.User}")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Equipment>> GetAllEquipment()
+        {
+            var role = HttpContext.Session.GetString("role");
+
+            var equipments = _equipmentService.GetAllEquipment();
+            EquipmentListViewModel equipmentsList = new EquipmentListViewModel()
+            {
+                Items = equipments,
+                IsAdmin = role == UserRole.Admin
+            };
+            return View("~/Views/Equipment/Index.cshtml", equipmentsList);
+        }
+
+        [Authorize(Roles = $"{UserRole.Admin}, {UserRole.User}")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Equipment>> GetAllAvailableEquipment()
+        {
+            var role = HttpContext.Session.GetString("role");
+
+            var equipments = _equipmentService.GetAllAvailableEquipment();
+            EquipmentListViewModel equipmentsList = new EquipmentListViewModel()
+            {
+                Items = equipments,
+                Filter = "Available",
+                IsAdmin = role == UserRole.Admin
+            };
+            return View("~/Views/Equipment/Index.cshtml", equipmentsList);
+        }
+
+        [Authorize(Roles = UserRole.Admin)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Equipment>> GetAllRentedEquipment()
+        {
+            var role = HttpContext.Session.GetString("role");
+
+            var equipments = _equipmentService.GetAllRentedEquipment();
+            EquipmentListViewModel equipmentsList = new EquipmentListViewModel()
+            {
+                Items = equipments,
+                Filter = "Rented",
+                IsAdmin = role == UserRole.Admin
+            };
+            return View("~/Views/Equipment/Index.cshtml", equipmentsList);
+        }
+
+        /* Rentals view */
+        [Authorize(Roles = UserRole.Admin)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Rental>> GetOverdueRentals()
+        {
+            var rentals = _rentalService.GetOverdueRentals();
+            return Content("TODO - rental index");
+        }
+
+        /* Customers view */
+        [Authorize(Roles = UserRole.Admin)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetAllCustomers()
+        {
+            var customers = _customerService.GetAllCustomers();
+            return View("~/Views/Customers/Index.cshtml", customers);
         }
     }
 }
